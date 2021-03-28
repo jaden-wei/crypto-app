83605f2c4b17b75814fb147503d19e6faa14b978
Jaden <jaden.wei2003@gmail.com>
2021-03-27 17:04:17 -0700

    currency dropdown, small change to search bar

--- src/components/pages/home/Coin.js
+++ src/components/pages/home/Coin.js
@@ -1,18 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Coin = ({ input, coinData }) => {
+const Coin = ({ input, coinData, currency }) => {
+  let currencySymbol = "$";
+
+  switch (currency) {
+    case "eur":
+      currencySymbol = '€'
+      break;
+    case 'gbp':
+      currencySymbol = '£'
+      break;
+    case 'cad':
+      currencySymbol = '$'
+      break;
+    case 'jpy':
+      currencySymbol = '¥'
+      break;
+    default:
+      break;
+  }
+
   if (input.length === 0 || coinData.id.includes(input)) {
     return (
       <tr className="coin-table-row">
         <td>
           <img src={coinData.image} alt="logo" className="coin-logo" />
-          <Link to={`/coinpage/${coinData.id}`}>
-            {coinData.name}
-          </Link>
+          <Link to={`/coinpage/${coinData.id}`}>{coinData.name}</Link>
           <span className="coin-symbol">{coinData.symbol}</span>
         </td>
-        <td>${Math.round(coinData.current_price * 100) / 100}</td>
+        <td>
+          {currencySymbol}
+          {Math.round(coinData.current_price * 100) / 100}
+        </td>
         <td
           className={
             coinData.price_change_percentage_24h > 0
@@ -22,7 +42,10 @@ const Coin = ({ input, coinData }) => {
         >
           {Math.round(coinData.price_change_percentage_24h * 100) / 100}%
         </td>
-        <td>${Math.round(coinData.market_cap * 100) / 100}</td>
+        <td>
+        {currencySymbol}
+          {Math.round(coinData.market_cap * 100) / 100}
+        </td>
         <td>{coinData.total_volume}</td>
       </tr>
     );
--- src/components/pages/home/List.js
+++ src/components/pages/home/List.js
@@ -10,14 +10,15 @@ import Search from "./Search";
 const List = () => {
   const [cryptoData, setCryptoData] = useState([]);
   const [input, setInput] = useState("");
+  const [currency, setCurrency] = useState('usd');
 
   useEffect(() => {
     updateData();
-  }, []);
+  }, [currency]);
 
   const updateData = async () => {
     const data = await axios.get(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h`
     );
 
     setCryptoData(data.data);
@@ -25,13 +26,36 @@ const List = () => {
 
   const renderCoinData = () => {
     return cryptoData.map((coin, index) => {
-      return <Coin input={input} key={index} coinData={cryptoData[index]} />;
+      return <Coin input={input} key={index} coinData={cryptoData[index]} currency={currency} />;
     });
   };
 
   return (
     <div className="coin-list">
-      <Search input={input} setInput={setInput}/>
+      <div className="coin-list-options">
+        <ReactBootStrap.DropdownButton
+          id="dropdown-basic-button"
+          title={currency.toUpperCase()}
+        >
+          <ReactBootStrap.Dropdown.Item onClick={() => {setCurrency('usd')}}>
+            USD (default)
+          </ReactBootStrap.Dropdown.Item>
+          <ReactBootStrap.Dropdown.Item onClick={() => {setCurrency('eur')}}>
+            EUR
+          </ReactBootStrap.Dropdown.Item>
+          <ReactBootStrap.Dropdown.Item onClick={() => {setCurrency('gbp')}}>
+            GBP
+          </ReactBootStrap.Dropdown.Item>
+          <ReactBootStrap.Dropdown.Item onClick={() => {setCurrency('cad')}}>
+            CAD
+          </ReactBootStrap.Dropdown.Item>
+          <ReactBootStrap.Dropdown.Item onClick={() => {setCurrency('jpy')}}>
+            JPY
+          </ReactBootStrap.Dropdown.Item>
+        </ReactBootStrap.DropdownButton>
+
+        <Search input={input} setInput={setInput} />
+      </div>
       <ReactBootStrap.Table variant="dark">
         <thead>
           <tr>
